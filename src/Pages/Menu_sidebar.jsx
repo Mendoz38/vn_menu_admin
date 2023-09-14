d72093bfc99f71c23386b3d769f1558a31727a26
Cédric Mendoza <cedric@mendoz.fr>
2023-09-14 11:57:02 +0200

    ajout + pour sous menu

--- src/Pages/Menu_sidebar.jsx
+++ src/Pages/Menu_sidebar.jsx
@@ -2,21 +2,26 @@ import { useState } from "react";
 import "./menu_sidebar.css";
 import items from "./../data/sidebar.json";
 
-
-const SidebarItem2 = ({ item }) => {
-  return (
-    <li>
-      <a href={item.path || "#"}>
-        <span>{item.title}</span>
-      </a>
-    </li>
-  );
-}
-
-const SidebarItem = ({ item }) => {
+const MenuNiveau1 = ({ item }) => {
   const [open, setOpen] = useState(false);
 
   if (item.childrens) {
+    if (item.childrens.length === 1) {
+      return (
+        <li>
+         <div className="iocn-link" >
+          <a href="#">
+            {item.icon && <i className={item.icon}></i>}
+            <span className="link_name">{item.title}</span>
+          </a>
+            {item.childrens.map((child, index) => (
+              <SimplePlus key={index} item={child} />
+            ))}
+        </div>
+        </li>
+      );
+    }
+    else {
     return (
       <li className={open ? "showMenu" : ""}>
         <div className="iocn-link" onClick={() => setOpen(!open)}>
@@ -28,11 +33,13 @@ const SidebarItem = ({ item }) => {
         </div>
         <ul className="sub-menu">
           {item.childrens.map((child, index) => (
-            <SidebarItem2 key={index} item={child} />
+            <MenuNiveau2 key={index} item={child} />
           ))}
         </ul>
       </li>
     );
+  }
+
   } else {
     return (
       <li>
@@ -43,37 +50,81 @@ const SidebarItem = ({ item }) => {
       </li>
     );
   }
-}
+};
+
+const MenuNiveau2 = ({ item }) => {
+  const [open, setOpen] = useState(false);
+
+  if (item.childrens) {
+    return (
+      <li className={open ? "showMenu" : ""}>
+        <div className="icon_plus" onClick={() => setOpen(!open)}>
+          <a href={item.path || "#"}>
+            <span>{item.title}</span>
+          </a>
+            {item.childrens.map((child, index) => (
+              <MenuNiveau3 key={index} item={child} />
+            ))}
+        </div>
+      </li>
+    );
+  } else {
+    return (
+      <li>
+        <a className="icon_plus" href={item.path || "#" }>
+          {item.title && <i className={item.title}></i>}
+        </a>
+      </li>
+    );
+  }
+};
+
+const MenuNiveau3 = ({ item }) => {
+  return (
+      <a href={item.path || "#"}>
+        {item.icon && <i className={`plus ${item.icon}`}></i>}
+      </a>
+   );
+};
+
+const SimplePlus = ({ item }) => {
+  return (
+      <a href={item.path || "#"}>
+        {item.icon && <i className={`simple_plus ${item.icon}`}></i>}
+      </a>
+  );
+};
 
 export default function Menu_sidebar() {
-  const [showMenu, setShowMenu] = useState(false);
-  const onClickShowMenu = () => {
-    setShowMenu(!showMenu);
-  };
 
   return (
     <>
-      <div className={`sidebar ${showMenu ? "" : "close"}`}>
-        <div className="logo-details" onClick={onClickShowMenu}>
-            <i className="bx bx-menu"></i>
+      <div className={`menu_sidebar`}>
+        <div className="logo-details">
+          <div id="icon_side"></div>
           <span className="logo_name">Vin Nat</span>
         </div>
 
         <ul className="nav-links">
           {items.map((item, index) => (
-            <SidebarItem key={index} item={item} />
+            <MenuNiveau1 key={index} item={item} />
           ))}
-          <div className="profile-details">
-            <div className="profile-content">image</div>
-            <div className="name-job">
-              <div className="profile_name">Prem Shahi</div>
-              <div className="job">Web Desginer</div>
-            </div>
-            <span className="deco">
-              <i className="bx bx-log-out"></i>
-            </span>
-          </div>
         </ul>
+        <div className="profile-details">
+          <div className="profile-content">
+            <img
+              src="https://www.vinsnaturels.fr/999_membres/img/faces/face-3.jpg"
+              alt="user"
+            />
+          </div>
+          <div className="name-job">
+            <div className="profile_name">Cédric Mendoza</div>
+            <div className="job">Web Desginer</div>
+          </div>
+          <span className="deco">
+            <i className="bx bx-log-out"></i>
+          </span>
+        </div>
       </div>
     </>
   );
